539e030aa4308f0dd5d8ed37d017910133407b63
unknown <sandipkhakural6@gmail.com>
2024-01-18 14:05:41 +0545

    first

---
+++ binary.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:-1].values
+y = dataset.iloc[:, -1].values
+from sklearn.tree import DecisionTreeRegressor
+regression=DecisionTreeRegressor(random_state=0)
+regression.fit(X,y)
+regression.predict([[6.5]])
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
